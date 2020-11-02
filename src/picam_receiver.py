6a308942b0756d2d54ad52938994199975b7b7e9
pgavriel <pgavriel@mail.middlesex.edu>
2020-11-01 20:00:58 -0500

    Receiver update

--- src/picam_receiver.py
+++ src/picam_receiver.py
@@ -10,19 +10,43 @@ from datetime import datetime
 
 from PIL import Image
 import cv2
+from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
+
+def get_time_string(include_date=False):
+    dt = datetime.now()
+    dt_string = ""
+    date = "{:02}-{:02}-{:02}".format(dt.month,dt.day,dt.year-2000)
+    time = "{:02}-{:02}-{:02}-{:06}".format(dt.hour,dt.minute,dt.second,dt.microsecond)
+    if include_date:
+        dt_string = "{}--{}".format(date,time)
+    else:
+        dt_string = time
+    return dt_string
+
+
 def callback(img):
-    print("Image received.")
+    node = img.header.frame_id
+    print("Image received from: {}".format(node))
+    label = "taskboard"
     bridge = CvBridge()
     try:
-        cv_image = bridge.imgmsg_to_cv2(img, "rgb8")
+        cv_image = bridge.imgmsg_to_cv2(img, "bgr8")
+        filename = '{}-{}-{}.png'.format(node,label,get_time_string())
+        image = Image.fromarray(cv_image)
+        image.save(filename)
+        print("Image saved to: {}".format(filename))
         cv2.imshow('test',cv_image)
     except CvBridgeError as e:
         print(e)
 
+
 def picam_receiver():
     rospy.init_node('picam_receiver')
+    #global SAVE_DIR
+    SAVE_DIR = '/home/pgavriel/ros_ws/src/ros_picam/captures'
+    os.chdir(SAVE_DIR)
     sub = rospy.Subscriber("picam_output",ROSImage,callback)
     rospy.loginfo("Subscriber up.")
     rospy.spin()

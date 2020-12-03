6786c7ddb97146e76de72a3a5eecbebed3ef03b2
pgavriel <pgavriel@mail.middlesex.edu>
2020-12-03 16:22:40 -0500

    Added arducam client with autofocus

--- src/picam_client_arducam.py
+++ src/picam_client_arducam.py
@@ -14,12 +14,15 @@ from time import sleep
 from datetime import datetime
 
 from picamera import PiCamera
+from picamera.array import PiRGBArray
 from PIL import Image
 import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import taskboard_detection as tb
 
+import smbus
+bus = smbus.SMBus(0)
 
 class picam_client:
     def __init__(self):
@@ -45,14 +48,14 @@ class picam_client:
         rospy.loginfo(str)
 
         # Camera Parameters
-        self.enable_video = rospy.get_param("~enable_video", True)
-        self.cam_w = rospy.get_param("~im_width", 1920)
-        self.cam_h = rospy.get_param("~im_height", 1080)
-        self.cam_fps = rospy.get_param("~fps", 30)
-        self.cam_brightness = rospy.get_param("~brightness", 50)
-        self.cam_contrast = rospy.get_param("~contrast", 0)
-        self.cam_iso = rospy.get_param("~iso", 200)
-        self.cam_rotation = rospy.get_param("~rotation", 0)
+        # self.enable_video = rospy.get_param("~enable_video", True)
+        # self.cam_w = rospy.get_param("~im_width", 1920)
+        # self.cam_h = rospy.get_param("~im_height", 1080)
+        # self.cam_fps = rospy.get_param("~fps", 30)
+        # self.cam_brightness = rospy.get_param("~brightness", 50)
+        # self.cam_contrast = rospy.get_param("~contrast", 0)
+        # self.cam_iso = rospy.get_param("~iso", 200)
+        # self.cam_rotation = rospy.get_param("~rotation", 0)
         # Create PiCamera object
         self.camera = self.setup_camera()
         rospy.loginfo("Camera initialized.")
@@ -199,15 +202,30 @@ class picam_client:
         self.cam_contrast = rospy.get_param("~contrast", 0)
         self.cam_iso = rospy.get_param("~iso", 200)
         self.cam_rotation = rospy.get_param("~rotation", 0)
+        self.cam_focus = rospy.get_param("~focus", 0)
         rospy.loginfo("Creating PiCamera object...")
         rospy.loginfo("Dim: {}x{}  FPS: {}".format(self.cam_w,self.cam_h,self.cam_fps))
         rospy.loginfo("Brightness: {}  Contrast: {}".format(self.cam_brightness,self.cam_contrast))
         rospy.loginfo("ISO: {}  Rotation: {}".format(self.cam_iso,self.cam_rotation))
+        rospy.loginfo("Focus: {}".format(self.cam_focus))
         camera = PiCamera(resolution=(self.cam_w, self.cam_h), framerate=self.cam_fps)
         camera.brightness = self.cam_brightness
         camera.contrast = self.cam_contrast
         camera.iso = self.cam_iso
         camera.rotation = self.cam_rotation
+
+        # Set camera focus
+        if self.cam_focus == 0:
+            self.autofocus(camera)
+            # Return to desired resolution
+            camera.resolution = (self.cam_w, self.cam_h)
+        elif self.cam_focus > 950:
+            self.set_focus(950)
+        elif self.cam_focus < 15:
+            self.set_focus(15)
+        else:
+            self.set_focus(self.cam_focus)
+
         # Wait for the automatic gain control to settle
         sleep(2)
         # Now fix the values
@@ -217,6 +235,86 @@ class picam_client:
         camera.awb_mode = 'off'
         camera.awb_gains = g
         return camera
+
+    #Arducam Specific Functions
+    def set_focus(self,val):
+        value = (val << 4) & 0x3ff0
+        data1 = (value >> 8) & 0x3f
+        data2 = value & 0xf0
+        # time.sleep(0.5)
+        rospy.loginfo("Focus Set: {}".format(val))
+        # bus.write_byte_data(0x0c,data1,data2)
+        os.system("i2cset -y 0 0x0c %d %d" % (data1,data2))
+
+    def calculate_focus(self,camera):
+        rawCapture = PiRGBArray(camera)
+        camera.capture(rawCapture,format="bgr", use_video_port=True)
+        image = rawCapture.array
+        rawCapture.truncate(0)
+        img_gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+        img_sobel = cv2.Laplacian(img_gray,cv2.CV_16U)
+        #img_sobel = cv2.Sobel(img_gray,cv2.CV_16U,1,1)
+        focus_score = cv2.mean(img_sobel)[0]
+        rospy.loginfo("Focus Score: {}".format(focus_score))
+        return focus_score
+
+    def autofocus(self,camera):
+        camera.resolution = (640, 480)
+        sleep(0.1)
+        rospy.loginfo("Starting Autofocus...")
+        max_index = 10
+        max_value = 0.0
+        last_value = 0.0
+        dec_count = 0
+        focal_distance = 10
+        while True:
+            # Set focus
+            self.set_focus(focal_distance)
+            sleep(0.1)
+            # Take image and calculate clarity
+            val = self.calculate_focus(camera)
+            # Find maximum image clarity
+            if val > max_value:
+                max_index = focal_distance
+                max_value = val
+            # If clarity starts to decrease
+            if val < last_value:
+                dec_count += 1
+            else:
+                dec_count = 0
+            # Image clarity is reducted for six consecutive frames
+            if dec_count > 6:
+                break
+            last_value = val
+            # Increase the focal distance
+            focal_distance += 15
+            if focal_distance > 1000:
+                break
+
+        rospy.loginfo("Max Index: {}, Max Value: {}\n".format(max_index,max_value))
+        rospy.loginfo("Fine Tuning...")
+        fine_lb = max_index - 25
+        fine_ub = max_index + 25
+        focal_distance = fine_lb
+        max_index = fine_lb
+        max_value = 0.0
+        rospy.loginfo("Range: {}-{}".format(fine_lb,fine_ub))
+        sleep(0.2)
+        while focal_distance <= fine_ub:
+            #Adjust focus
+            self.set_focus(focal_distance)
+            sleep(0.1)
+            #Take image and calculate image clarity
+            val = self.calculate_focus(camera)
+            #Find the maximum image clarity
+            if val > max_value:
+            	max_index = focal_distance
+            	max_value = val
+            focal_distance += 5
+
+        self.set_focus(max_index)
+        rospy.loginfo("Max Index: {}, Max Value: {}\n".format(max_index,max_value))
+        sleep(0.1)
 
 
 if __name__ == '__main__':
